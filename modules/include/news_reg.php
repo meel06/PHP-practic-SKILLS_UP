72ef1c04afd522c20062f84daa841d5f82ed2bf2
Melanie Ru <melanie.pfaeskillsup@gmail.com>
2022-10-03 09:39:04 +0100

    Chequear fallo de validación y reestructurar código, pendiente de terminar

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -1,7 +1,7 @@
 <?php
 require '../require/config.php';
 
-$name = $email = $phone = $address = $city = $communities = $Zcode = $format = $newscheck = $text = "";
+$name = $email = $phone = $address = $city = $communities = $Zcode = $format = $newscheck = $newsletter = "";
 
 // Funciones :
 
@@ -26,11 +26,20 @@ function validar_name($name)
 
 function validar_phone($phone)
 {
-    if (!preg_match('/^[0-9] {10}+$/', $phone)) {
+    if (!preg_match('/^[0-9] {9}+$/', $phone)) {
         return false;
     } else {
         return true;
 
+    }
+}
+
+function validar_email($email)
+{
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return false;
+    } else {
+        return true;
     }
 }
 
@@ -52,27 +61,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $address = limpiar_dato($_POST["address"]);
         $city = limpiar_dato($_POST["city"]);
-        $communities = limpiar_dato($_POST["Autonomous communities"]);
+        $communities = limpiar_dato($_POST["communities"]);
         $Zcode = limpiar_dato($_POST["Zcode"]);
         $format = limpiar_dato($_POST["format"]);
         $newscheck = limpiar_dato($_POST["newscheck"]);
         $text = limpiar_dato($_POST["text"]);
+
     }
 }
-// Terminar las validaciones.
-/**
 
- */
+var_dump($newsletter);
 
-// Algoritmo Enviar formulario
-/*/
-Si (llega datos) Entonces
+$othert = limpiar_dato($_POST["othert"]);
+echo "<strong>Noticias que quiere recibir: $newsletter";
+var_dump($name);
+
+// ==========================  BORRAME
+echo "<br><strong>Name:</strong> $name <br>";
+echo "<strong>Telefono:</strong> $phone <br>";
+echo "<strong>Email: </strong> $email <br>";
+
+if (validar_name($name)) {
+    echo "validada";
+} else {
+    echo "no valida";
+}
+
+// ======================  BORRAME
+
+/* Si (llega datos) Entonces
 tratamos datos
 Si si hay información Entonces
-limpiar la información. validar la informaciñon. Si datos necesarios Entonces
-asegurar de que están bien escrito. Sino
-mandamos dato tal cual. Fin Si
-Mostrar que todos los datos son correctos para enviar a BBDD. Sino
-enviar datos necesarios Fin Si SiNo
-avisar no han llegado. Fin Si FinAlgoritmo
-/*/
+Si no llegan variables?**
+Asignarles NULL.
+limpiar la información. check!!
+validar la informacinon.
+Si datos necesarios Entonces
+asegurar de que están bien escrito.
+SiNo
+mandamos dato tal cual.
+Fin Si
+Mostrar que todos los datos son correctos para enviar a BBDD.
+SiNo
+enviar datos necesarios
+Fin Si
+SiNo
+avisar no han llegado.
+Fin Si */
+?>
