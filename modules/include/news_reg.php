19f05143d8b2817daeeb5c77418b9336854cd82f
Melanie Ru <melanie.pfaeskillsup@gmail.com>
2022-10-04 11:01:36 +0100

    Añadí el código que me faltaba por terminar

--- modules/include/news_reg.php
+++ modules/include/news_reg.php
@@ -48,15 +48,63 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //  echo "<br><strong>Metodo post enviado</strong><br>";
     // Variables requeridas para enviar a Base de datos: name, email y phone.
     if (!empty($_POST["name"]) || !empty($_POST["email"]) || !empty($_POST["phone"])) {
-        echo "<br><strong>name post hay datos</strong><br>";
+        echo "<br><strong>En los campos requeridos hay datos</strong><br>";
         $name = limpiar_dato($_POST["name"]);
         $email = limpiar_dato($_POST["email"]);
         $phone = limpiar_dato($_POST["phone"]);
+
+        
+        if (validar_name($name)){
+            echo "La validación de Name es correcta<br>";
+        } else{
+            $name_err = true;
+        }
+
+        if (validar_email($email)){
+            echo "La validación de Email es correcta<br>";
+        } else{
+            $email_err = true;
+        }
+
+        if (validar_phone($phone)){
+            echo "La validación de Teléfono es correcta<br>";
+        } else{
+            $phone_err = true;
+        }
+
+        // Condición si no se cumple alguna validación, parar código
+        if (validar_name($name) && validar_email($correo) && validar_phone($phone)){
+
+            
+            // A las variables que pueden ser NULL añadir if para guardar en BBDD como NULL
 
         if (isset($_POST["address"])) {
             $address = limpiar_dato($_POST["address"]);
         } else {
             $address = null;
+        }
+        if (isset($_POST["city"])){
+            $city = limpiar_dato($_POST["city"]);
+        } else{
+            $city = NULL;
+        }
+        
+        if (isset($_POST["communities"])){
+            $communities = limpiar_dato($_POST["communities"]);
+        } else{
+            $communities = NULL;
+        }
+        
+        if (isset($_POST["Zcode"])){
+            $Zcode = limpiar_dato($_POST["Zcode"]);
+        } else{
+            $Zcode = NULL;
+        } 
+        
+        if (isset($_POST["newscheck"])){
+            $newscheck = limpiar_dato($_POST["newscheck"]);
+        } else{
+            $newscheck = NULL;
         }
 
         $address = limpiar_dato($_POST["address"]);
@@ -70,21 +118,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-var_dump($newsletter);
+  
+if (isset($_POST["topics"])){
+    $topics = limpiar_dato($_POST["topics"]);
+} else{
+    $topics = NULL;
+}
+// ============================================================= BORRAME
 
-$othert = limpiar_dato($_POST["othert"]);
-echo "<strong>Noticias que quiere recibir: $newsletter";
-var_dump($name);
+echo "<br><strong>Name: </strong>".$name ."<br>";
 
-// ==========================  BORRAME
-echo "<br><strong>Name:</strong> $name <br>";
-echo "<strong>Telefono:</strong> $phone <br>";
-echo "<strong>Email: </strong> $email <br>";
+echo "<br><strong>Email: </strong>".$email ."<br>";
 
-if (validar_name($name)) {
-    echo "validada";
+echo "<br><strong>Teléfono: </strong>".$phone ."<br>";
+
+// ============================================================= BORRAME
+} else{
+if ($name_err == true){
+    echo "La validación de Nombre ha fallado<br>";
+} elseif ($email_err == true) {
+    echo "La validación de Email ha fallado<br>";
+} elseif ($phone_err == true) {
+    echo "La validación de Teléfono ha fallado<br>";
+}
+}
 } else {
-    echo "no valida";
+echo "Uno de los datos requeridos no ha sido rellenado";
+}
+} else{
+echo "No hemos recibido por el métedo POST";
 }
 
 // ======================  BORRAME
